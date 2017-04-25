e98be025db1d2239c6e34348c9f2835bf1fbc966
Daniel Kim <daniel10301994@hotmail.com>
2017-04-25 18:42:35 +1200

    Add close button

--- src/FoodCard.js
+++ src/FoodCard.js
@@ -3,7 +3,6 @@ import AppBar from 'material-ui/AppBar';
 import PropTypes from 'prop-types';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import muiThemeable from 'material-ui/styles/muiThemeable';
-import IconButton from 'material-ui/IconButton';
 import SocialCake from 'material-ui/svg-icons/social/cake';
 import FlatButton from 'material-ui/FlatButton';
 import Left from './Left';
@@ -13,6 +12,9 @@ import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 import Visibility from 'material-ui/svg-icons/action/visibility';
 import VisibilityOff from 'material-ui/svg-icons/action/visibility-off';
 import Toggle from 'material-ui/Toggle';
+import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
 export default class FoodCard extends Component {
 
 
@@ -44,6 +46,9 @@ export default class FoodCard extends Component {
           <img src={require('../public/images/apple.jpg')} style={{ borderTopLeftRadius: '1rem', borderBottomLeftRadius: '1rem', width:'50%', float:'left', overflow:'hidden' }} />
 
         <div>
+        <div style={{ float:'right', width:'50%' }}>
+          <IconButton style={{float:'right' }} ><NavigationClose /></IconButton>
+        </div>
           <CardTitle title={name} subtitle={category} style={{ padding:'0' }}/>
           <CardText style={{ textAlign:'center', padding:'0' }}>
             <p><b>Portion Size:</b> {portionSize}</p>
